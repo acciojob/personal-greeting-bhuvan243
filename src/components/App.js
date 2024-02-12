3e561b738e5d14be13cc82f89db2ac414e3af214
Bhuvaneshwaran <125134187+bhuvan243@users.noreply.github.com>
2024-02-12 17:44:37 +0000

    fix2

--- src/components/App.js
+++ src/components/App.js
@@ -7,8 +7,11 @@ const App = () => {
   return (
     <div>
         {/* Do not remove the main div */}
-        <p>Enter your name:</p>
-        <input type="text" onChange={(e) => setName(e.target.value)}/>
+        <form>
+          <label>Enter your name:</label>
+          <input type="text" onChange={(e) => setName(e.target.value)}/>
+        </form>
+        
         { name && <p>Hello {name}!</p> }
     </div>
   )
